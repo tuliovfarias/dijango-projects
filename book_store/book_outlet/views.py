a925ffecc6bcb94088c2534419d4533150aaeec9
Túlio Farias <tulio-4ever@hotmail.com>
2021-07-04 21:51:35 -0300

    #94

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from .models import Book
+from django.db.models import Avg
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all().order_by('title') #'-title' (desc)
+    num_books= books.count()
+    avg_rating=books.aggregate(Avg("rating"))
+
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_books":num_books,
+        "avg_rating":avg_rating
+    })
+
+
+def book_detail(request, slug):
+    try:
+        book = Book.objects.get(slug=slug)
+    except:
+        raise Http404()
+    #book=get_object_or_404(Book, slug=slug)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
+    })
